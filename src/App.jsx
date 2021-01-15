5152698a8b605c7904cd3844408b185498833325
雷永亮 <979700942@qq.com>
2021-01-15 14:08:29 +0800

    更新

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,13 @@ import { textState, theme } from "./store/atom";
 
 import { useTranslation, Trans } from "react-i18next";
 
+import TagCloud1 from 'react-tag-cloud';
+import randomColor from 'randomcolor';
+
+import { TagCloud } from 'react-tagcloud'
+import WordCloud3d from 'react-d3-cloud';
+
+
 
 function App() {
   const [visible, setVisible] = useState(false);
@@ -67,8 +74,65 @@ function App() {
 
   const { t, i18n } = useTranslation('page')
 
-  
+  const data = [
+    { value: '方法', count: 38 },
+    { value: '的发v的', count: 30 },
+    { value: '白板笔', count: 28 },
+    { value: ' 所处的菜市场', count: 25 },
+    { value: '与的发v的', count: 33 },
+    { value: '万二恶女', count: 18 },
+    { value: '预约反对党', count: 20 },
+  ]
 
+  const customRenderer = (tag, size, color) => {
+    return (
+      <span key={tag.value} style={{ color: color, fontSize: size, borderColor: color }} className={`tag-${size} tag-item`}>
+        #{tag.value}
+      </span>
+    )
+  }
+
+  const data2 = [
+    { text: '的发v到访', value: 1000 },
+    { text: '个方便更方便', value: 200 },
+    { text: '发该不该发布', value: 800 },
+    { text: '付办公费', value: 1000000 },
+    { text: '股份不规范', value: 10 },
+  ];
+   
+  const fontSizeMapper = word => Math.log2(word.value) * 5;
+  const rotate = word => word.value % 360;
+
+  useEffect(() => {
+    var options = {
+      "list": [
+          ['的发v到访', 10],
+          ['发v', 9],
+          ['说说', 7],
+          ['报备', 6],
+          ['问问', 4],
+          ['VB广发', 5],
+          ['寂寞寂寞', 4],
+          ['咯loloil是的发v', 3],
+          ['是发大V发v到访', 3]
+      ],
+      "gridSize": 6, // size of the grid in pixels
+      "weightFactor": 4, // number to multiply for size of each word in the list
+      "fontWeight": 'normal', // 'normal', 'bold' or a callback
+      "fontFamily": '微软雅黑', // font to use
+      "color": (word, weight, fontSize, distance, theta) => {
+        return 'red'
+      }, // 'random-dark' or 'random-light'
+      "backgroundColor": '#fff', // the color of canvas
+      "minRotation": 90,
+      "maxRotation": 90,
+      "shape": "diamond",
+      "shuffle": "",
+      "rotateRatio": 0.5,
+  }
+    var canvas = document.getElementById('canvas');
+    window.WordCloud(canvas, options);
+    }, [])
   return (
     
     <ThemeProvider theme={themeObj}>
@@ -120,7 +184,41 @@ function App() {
 
         <button onClick={() => i18n.changeLanguage('en')}>英文</button>
         <button onClick={() => i18n.changeLanguage('zh')}>中文</button>
+        <TagCloud1 
+          style={{
+            fontFamily: '微软雅黑',
+            fontSize: 12,
+            padding: 5,
+            color: () => randomColor(),
+            width: '300px',
+            height: '300px'
+          }}>
+        {data.map((item, index) => <div key={index} style={{fontSize: item.count}}>{item.value}</div>)}
+      </TagCloud1>
+
+      <TagCloud
+          className="tag-box"
+          minSize={12}
+          maxSize={28}
+          tags={data}
+          renderer={customRenderer}
+          onClick={tag => alert(`'${tag.value}' was selected!`)}
+        />
+
+      <div id="canvas-container" align="center">
+          <canvas id="canvas" width="600px" height="400px"></canvas>
       </div>
+
+      <WordCloud3d
+        data={data2}
+        fontSizeMapper={fontSizeMapper}
+        rotate={rotate}
+        padding={4}
+        font='微软雅黑'
+      />
+      
+      </div>
+      
     </ThemeProvider>
   );
 }
